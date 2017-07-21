dd2f98ee61ab64499bbe6a75290b4425bc945acb
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-20 23:55:33 -0400

    Added README and updated test to be more interesting

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 int main() {
   json record;
   
-  record["projects"]["json"]["year"] = 2017;                                             // value of type int  
-  record["projects"]["json"]["url"] = "https://github.com/pranav-srinivas-kumar/json";   // value of type string
-  record["projects"]["json"]["commits/day"] = 5.21;                                      // value of type string
+  record["projects"]["json"]["year"] = 2017;                                           // value of type int  
+  record["projects"]["json"]["url"] = "https://github.com/pranav-srinivas-kumar/json"; // value of type string
+  record["projects"]["json"]["commits/day"] = 5.21;                                    // value of type string
   
-  auto keys = record["projects"]["json"].keys();                                         // ["year", "url", "commits/day"]
-  auto query = record["projects"]["json"]["commits/day"].value();                        // query = "5.21"
+  auto keys = record["projects"]["json"].keys();                                       // ["year", "url", "commits/day"]
+  auto query = record["projects"]["json"]["commits/day"].value();                      // query = "5.21"
   
-  record.print();                                                                        // pretty print of json data
+  record.print();                                                                      // pretty print of JSON
   /*
   {
       "projects" :
--- json/main.cpp
+++ json/main.cpp
@@ -3,16 +3,26 @@
 int main() {
   json record;
 
-  record["projects"]["json"]["year"] = 2017;                                             // value of type int  
-  record["projects"]["json"]["url"] = "https://github.com/pranav-srinivas-kumar/json";   // value of type string
-  record["projects"]["json"]["commits/day"] = 5.21;                                      // value of type string
+  record["projects"]["json"]["year"] = 2017;                                           // value of type int  
+  record["projects"]["json"]["url"] = "https://github.com/pranav-srinivas-kumar/json"; // value of type string
+  record["projects"]["json"]["commits/day"] = 5.21;                                    // value of type string
 
-  auto keys = record["projects"]["json"].keys();                                         // ["year", "url", "commits/day"]
-  auto query = record["projects"]["json"]["commits/day"].value();                        // query = "5.21"
+  auto keys = record["projects"]["json"].keys();                                       // ["year", "url", "commits/day"]
+  auto query = record["projects"]["json"]["commits/day"].value();                      // query = "5.21"
 
   record.print();
   /*
-
+  {
+      "projects" :
+      {
+          "json" :
+          {
+              "commits/day" : "5.21",
+              "url" : "https://github.com/pranav-srinivas-kumar/json",
+              "year" : "2017"
+          }
+      }
+  }
   */
   system("PAUSE");
 }
