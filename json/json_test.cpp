c2aaf28307f1ec3b60317bd4202519c2acf88bb7
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-21 00:00:07 -0400

    Added json_test.cpp

---
+++ json/json_test.cpp
@@ -0,0 +1,28 @@
+#include "json.h"
+
+int main() {
+  json record;
+
+  record["projects"]["json"]["year"] = 2017;                         // value of type int  
+  record["projects"]["json"]["url"] = "https://json.org";            // value of type string
+  record["projects"]["json"]["commits/day"] = 5.21;                  // value of type double
+
+  auto keys = record["projects"]["json"].keys();                     // ["year", "url", "commits/day"]
+  auto query = record["projects"]["json"]["commits/day"].value();    // query = "5.21"
+
+  record.print();
+  /*
+  {
+      "projects" :
+      {
+          "json" :
+          {
+              "commits/day" : "5.21",
+              "url" : "https://json.org",
+              "year" : "2017"
+          }
+      }
+  }
+  */
+  system("PAUSE");
+}
