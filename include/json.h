e5dcf8399bc0864fbf7a21d90b3b70a4e9f41499
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2017-07-21 23:05:58 -0400

    Added destructor and renamed data to object

--- include/json.h
+++ include/json.h
@@ -11,16 +11,19 @@
 class json {
 public:
   // Constructors
-  json() {}
+  json();
+
+  // Destructors
+  ~json();
 
   // Overloaded Operators
   json& operator[](std::string key);  // overloaded index/subscript operator
 
-  json& operator=(const char * rhs);  // assign to const char array
-  json& operator=(std::string rhs);   // assign to string
-  json& operator=(int rhs);           // assign to integer
-  json& operator=(double rhs);        // assign to double
-  json& operator=(bool rhs);          // assign to boolean
+  json& operator=(const char * rhs);  // assign const char array
+  json& operator=(std::string rhs);   // assign string
+  json& operator=(int rhs);           // assign integer
+  json& operator=(double rhs);        // assign double
+  json& operator=(bool rhs);          // assign boolean
 
   // Getters
   auto keys();                        // returns json keys at current level
@@ -30,36 +33,47 @@ public:
   std::string to_string(int indent = 4, 
     bool comma_on_leaf = false, 
     bool comma_on_bracket = false);   // returns a string of the json for archiving
-  void print();                       // pretty prints json data
+  void print();                       // pretty prints json object
   void write(std::string file_path);  // writes json to file at provided path
 
 private:
-  std::map<std::string, json*> data;
-  //std::string json_string;
+  std::map<std::string, json*> object;
 };
+
+// default constructor
+json::json() {}
+
+// destructor
+json::~json() {
+  for (auto& pair : object) {
+    if (pair.second != nullptr) {
+      delete pair.second;
+    }
+  }
+}
 
 // overloaded index/subscript operator
 json& json::operator[](std::string key) {
-  if (data.find(key) != data.end()) {
-    return *data[key];
+  if (object.find(key) != object.end()) {
+    return *object[key];
   }
   else {
     json * value = new json();
-    data.insert(std::pair<std::string, json*>(key, value));
+    object.insert(std::pair<std::string, json*>(key, value));
     return *value;
   }
 }
 
 json& json::operator=(const char * rhs) {
   if (rhs != nullptr)
-    data.insert(std::pair<std::string, json*>(std::string(rhs), nullptr));
+    object.insert(std::pair<std::string, json*>(std::string(rhs), nullptr));
   else
-    data.insert(std::pair<std::string, json*>("null", nullptr));
+    object.insert(std::pair<std::string, json*>("null", nullptr));
   return (*this);
 }
 
 json& json::operator=(std::string rhs) {
-  data.insert(std::pair<std::string, json*>(rhs, nullptr));
+  object.insert(std::pair<std::string, json*>(rhs, nullptr));
   return (*this);
 }
 
@@ -67,7 +81,7 @@ json& json::operator=(std::string rhs) {
 json& json::operator=(int rhs) {
   std::ostringstream convert;
   convert << rhs;
-  data.insert(std::pair<std::string, json*>(convert.str(), nullptr));
+  object.insert(std::pair<std::string, json*>(convert.str(), nullptr));
   return (*this);
 }
 
@@ -75,21 +89,21 @@ json& json::operator=(int rhs) {
 json& json::operator=(double rhs) {
   std::ostringstream convert;
   convert << rhs;
-  data.insert(std::pair<std::string, json*>(convert.str(), nullptr));
+  object.insert(std::pair<std::string, json*>(convert.str(), nullptr));
   return (*this);
 }
 
 // json value can be boolean
 json& json::operator=(bool rhs) {
     std::string bool_str = (rhs == true)? "true" : "false";
-    data.insert(std::pair<std::string, json*>(bool_str, nullptr));
+    object.insert(std::pair<std::string, json*>(bool_str, nullptr));
   return (*this);
 }
 
 // returns a vector of keys at the current level of the json
 auto json::keys() {
   std::vector<std::string> result;
-  for (std::map<std::string, json*>::iterator it = data.begin(); it != data.end(); ++it) {
+  for (std::map<std::string, json*>::iterator it = object.begin(); it != object.end(); ++it) {
     result.push_back(it->first);
   }
   return result;
@@ -98,8 +112,8 @@ auto json::keys() {
 // returns value of json at current level
 std::string json::value() {
   std::string result = "";
-  if (data.size() == 1) {
-    std::map<std::string, json*>::iterator it = data.begin();
+  if (object.size() == 1) {
+    std::map<std::string, json*>::iterator it = object.begin();
     result = it->first;
   }
   return result;
@@ -112,7 +126,7 @@ std::string json::to_string(int indent,
   std::string indent_str(indent - 4, ' ');
   bool leaf_node = false;
 
-  for (auto& entry : data) {
+  for (auto& entry : object) {
     if (entry.second == nullptr) {
       leaf_node = true;
       if (entry.first == "true" || entry.first == "false")
@@ -132,11 +146,11 @@ std::string json::to_string(int indent,
     json_string += indent_str + "{\n";
   }
 
-  for (auto it = data.begin(); it != data.end(); ++it) {
+  for (auto it = object.begin(); it != object.end(); ++it) {
     if (it->second != nullptr) {
       indent_str = std::string(indent, ' ');
       json_string += indent_str + '"' + it->first + '"' + " : ";
-      if (std::next(it, 1) != data.end()) {
+      if (std::next(it, 1) != object.end()) {
         json_string += it->second->to_string(indent + 4, true, true);
       }
       else {
@@ -158,7 +172,7 @@ std::string json::to_string(int indent,
   return json_string;
 }
 
-// pretty prints json data
+// pretty prints json object
 void json::print() {
   std::cout << to_string() << std::endl;
 }
--- tests/json_test.cpp
+++ tests/json_test.cpp
@@ -1,27 +1,29 @@
 #include <json.h>
 
 int main() {
-  json record;
 
-  record["projects"]["json"]["year"] = 2017;                         
-  record["projects"]["json"]["url"] = "https://json.org";            
-  record["projects"]["json"]["commits/day"] = 5.21;
-  record["projects"]["json"]["is this cool?"] = true;
+  {
+    json record;
 
-  record["projects"]["llama"]["language"] = "C++";
-  record["projects"]["llama"]["domain"] = "Linear Algebra";
-  record["projects"]["llama"]["url"] = nullptr;
+    record["projects"]["json"]["year"] = 2017;                         
+    record["projects"]["json"]["url"] = "https://json.org";            
+    record["projects"]["json"]["commits/day"] = 5.21;
+    record["projects"]["json"]["is this cool?"] = true;
 
-  record["projects"]["rosmod"]["language"] = "C++/Python";
-  record["projects"]["rosmod"]["url"] = "https://github.com/rosmod";
-  record["projects"]["rosmod"]["active?"] = false;
+    record["projects"]["llama"]["language"] = "C++";
+    record["projects"]["llama"]["domain"] = "Linear Algebra";
+    record["projects"]["llama"]["url"] = nullptr;
 
-  auto keys = record["projects"]["json"].keys();                     // ["year", "url", "commits/day"]
-  auto query = record["projects"]["json"]["commits/day"].value();    // query = "5.21"
+    record["projects"]["rosmod"]["language"] = "C++/Python";
+    record["projects"]["rosmod"]["url"] = "https://github.com/rosmod";
+    record["projects"]["rosmod"]["active?"] = false;
 
-  std::string record_string = record.to_string();                    // returns a string of the json
-  record.print();                                                    // pretty prints json data
-  record.write("clone.json");                                        // writes json to file
-  
+    auto keys = record["projects"]["json"].keys();                     // ["year", "url", "commits/day"]
+    auto query = record["projects"]["json"]["commits/day"].value();    // query = "5.21"
+
+    std::string record_string = record.to_string();                    // returns a string of the json
+    record.print();                                                    // pretty prints json data
+    record.write("clone.json");                                        // writes json to file
+  }
   system("PAUSE");
 }
